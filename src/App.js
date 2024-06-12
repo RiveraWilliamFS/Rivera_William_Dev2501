0f1bafd36fbe985bb92fc29b53d3e8c85ccb0918
William <wdrivera@student.fullsail.edu>
2024-06-12 14:36:02 -0400

    Button added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { component} from 'react';
+import React, { Component } from 'react';
 import Button from './components /Button';
 
-class App extends component {
-  render(){
-
-  return (
-    <div className="App">
-   <p>Testing</p>
-    </div>
-  );
- }
+class App extends Component {
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1>Buttons:</h1>
+        <div>
+          <Button btnText="Post" />
+        </div>
+      </div>
+    );
+  }
 }
+
+const styles = {
+  container: {
+    padding: '20px',
+  }
+};
+
 export default App;
+
+
+
--- src/components /Button.js
+++ src/components /Button.js
@@ -1,11 +1,24 @@
-import react from 'react';
+import React from 'react';
 
-const Button = ({ text, onClick }) => {
-    return (
-        <button onClick={onClick}>
-            {text}
-        </button>
-    );
+const Button = (props) => {
+  return (
+    <button style={styles.redBtn}>
+      {props.btnText}
+    </button>
+  );
+}
+
+const styles = {
+  redBtn: {
+    fontWeight: 'bold',
+    backgroundColor: 'rgb(143, 202, 249)',
+    color: '#0066CC',
+    padding: '1%',
+    margin: '1%',
+    borderRadius: '10%',
+    border: 'none',
+    cursor: 'pointer'
+  }
 };
 
 export default Button;
